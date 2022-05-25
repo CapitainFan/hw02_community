9f8d8643a0efdd956f270ddb9d88cada8c90edb2
CapitainFan <gudokrulit@mail.ru>
2022-05-25 22:18:57 +0300

    yatube first try

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,24 +6,20 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
+    class Meta:
+        ordering = ['-pub_date']
+    name = models.CharField(max_length=200, default='unknown')
     text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
-    group = models.ForeignKey(
-        Group,
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE
-    )
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
+                              related_name="posts", blank=True, null=True)
