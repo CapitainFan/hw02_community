f8929a58f04e51b9c3678dc30b3f3cc323cdf8a7
CapitainFan <gudokrulit@mail.ru>
2022-05-26 21:42:15 +0300

    yatube first try

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -14,12 +14,10 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    class Meta:
-        ordering = ['-pub_date']
-    name = models.CharField(max_length=200, default='unknown')
     text = models.TextField()
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="posts")
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
-                              related_name="posts", blank=True, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
