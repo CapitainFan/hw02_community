918506860584d7eaadcde0f5083c5f347cd1eb22
CapitainFan <gudokrulit@mail.ru>
2022-05-26 20:34:47 +0300

    yatube first try

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Yatube.urls'
+ROOT_URLCONF = 'yatube.urls'
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
