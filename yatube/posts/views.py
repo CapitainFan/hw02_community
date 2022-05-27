7b6e580a72993a3bb62714b39b3873cad8735aac
CapitainFan <gudokrulit@mail.ru>
2022-05-27 15:48:11 +0300

    yatube first try

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,8 +3,8 @@ from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.all()[:10]
-    return render(request, 'posts/index.html', {'post': latest})
+    latest = Post.objects.all()[::-1]
+    return render(request, 'posts/index.html', {'post': latest[:10]})
 
 
 def group_posts(request, slug):
