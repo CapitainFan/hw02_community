cebf895ebca7c710b4e06116db644cbaf67ad1ed
CapitainFan <gudokrulit@mail.ru>
2022-05-27 13:53:49 +0300

    yatube first try

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,15 +8,6 @@ def index(request):
 
 
 def group_posts(request, slug):
-    template = 'posts /group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    title = Group.__str__
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    context = {
-        'posts': posts,
-        'title': title,
-    }
-    return render(request, template, context)
+    posts = Post.objects.filter(group=group)[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
