0bd2f9bf02f90e58eb4121db04f2cea79fbde9d8
CapitainFan <gudokrulit@mail.ru>
2022-05-30 10:26:02 +0300

    review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -11,6 +11,6 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:count]
+    posts = Post.objects.all()[:count]
     return render(request, 'posts/group_list.html',
                   {'group': group, 'posts': posts})
