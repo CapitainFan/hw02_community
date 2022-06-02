b26eebd659a15e74acf94f4dd4f0cbf27623fe02
CapitainFan <gudokrulit@mail.ru>
2022-06-02 15:14:33 +0300

    25th topic

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,10 +4,29 @@ from .models import Post, Group
 count = 10
 
 
-def index(request):
-    latest = Post.objects.all()[:count]
-    return render(request, 'posts/index.html', {'posts': latest})
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Post
 
+
+def index(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядить так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10) 
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
